01f5f1f94a2032cb8f921c6fc2aff7c4c9cd55ee
Divyesh-Padhiyar <divyeshpadhiyar285@gmail.com>
2024-09-12 10:03:50 +0000

    Type converted

---
+++ Js_Basics/Type_Converstion.js
@@ -0,0 +1,20 @@
+// Type Conversion
+
+let number = "33"
+
+console.log(number);
+console.log(typeof number);
+
+// Converted in String
+
+let numberToString = String(number);
+console.log(numberToString);
+console.log(typeof numberToString);
+
+// Converted in Boolean
+
+let numberToBoolean = Boolean(number);
+console.log(numberToBoolean);
+
+
+
