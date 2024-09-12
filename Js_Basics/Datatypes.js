062186037b88a6ced37aab17e231301b8f2b1b2d
Divyesh-Padhiyar <divyeshpadhiyar285@gmail.com>
2024-09-12 14:56:46 +0530

    Create Datatypes.js

---
+++ Js_Basics/Datatypes.js
@@ -0,0 +1,10 @@
+// datatypes
+
+let number = 123
+let name = "Divyesh"
+let stringName = 'Sonal'
+let booleantype = true
+let undefined
+let NoValue = null
+
+console.table([number,name,stringName,booleantype,undefined,NoValue]);
